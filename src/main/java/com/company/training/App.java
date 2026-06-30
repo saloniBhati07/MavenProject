15967f9ed76aa543aa3774d8313d9b87d9085080
saloniBhati07 <saloni.bha07@gmail.com>
2026-07-01 00:54:56 +0530

    Day11 Maven Project Setup

---
+++ src/main/java/com/company/training/App.java
@@ -0,0 +1,11 @@
+package com.company.training;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+      Employee emp= new Employee(101,"Saloni",60000);
+      emp.Displaydetails();
+    }
+}
---
+++ src/main/java/com/company/training/Employee.java
@@ -0,0 +1,18 @@
+package com.company.training;
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+    public Employee(int id, String name, double salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+    public void Displaydetails() {
+        System.out.println("Employee ID:"+id);
+        System.out.println("Employee Name:"+name);
+        System.out.println("EMployee Salary:"+salary);
+        
+    }
+    
+}
